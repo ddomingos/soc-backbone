9ada21dd9cf9ccc1f5ab4fb99d3284f426374512
Jeff <jeff.david.green@gmail.com>
2014-06-09 17:00:20 -0400

    initial push.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 soc-backbone
 ============
+
+Wiki ToDo list
+
+1. Install grunt and use it to help out with the installation of resources
---
+++ js/modules/movies.js
@@ -0,0 +1,61 @@
+var MovieModel = Backbone.Model.extend({
+    urlRoot: 'http://localhost:3000/movies',
+    parse: function(response) {
+        if (response.length) response = response[0];
+        response.id = response._id;
+        delete response._id;
+        return response;
+    },
+    toJSON: function() {
+        var attrs = _.clone(this.attributes);
+        attrs = _.omit(attrs, 'id');
+        return attrs;
+    }
+});
+
+var MovieView = Backbone.View.extend({
+    tagName: 'li',
+    className: 'movie',
+    template: _.template('<p><strong><%= title %></strong> (<%= year %>)</span></p><p><%= role %></p>'),
+    initialize: function() {
+        this.model.on('change', this.render, this);  
+    },
+    render: function () {
+        var attributes = this.model.attributes;
+        this.$el.html(this.template(attributes));
+        return this;
+    }
+});
+
+var MovieList = Backbone.Collection.extend({
+    url: 'http://localhost:3000/movies',
+    model: MovieModel,
+});
+
+var movieList = new MovieList();
+
+var MovieListView = Backbone.View.extend({
+    tagName: 'ul',
+    className: 'movie-list',
+    initialize: function() {
+        this.collection.on('add', this.addOne, this);
+        this.collection.on('reset', this.addAll, this);
+    },
+    render: function() {
+        this.addAll();
+    },
+
+    addOne: function(movieItem) {
+        var movieView = new MovieView({ model: movieItem });
+        console.log(movieView.render().el);
+        this.$el.append(movieView.render().el);
+    },
+
+    addAll: function() {
+        this.collection.forEach(this.addOne, this);  
+    },
+});
+
+var movieListView = new MovieListView({ collection: movieList });
+
+$('#all-movies').append(movieListView.el);
